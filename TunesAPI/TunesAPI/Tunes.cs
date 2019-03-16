165c741d44044e50f1807737f4987610d104f821
ceiran316 <x00132492@gmail.com>
2019-03-16 05:12:56 +0000

    list added

--- TunesAPI/TunesAPI/Tunes.cs
+++ TunesAPI/TunesAPI/Tunes.cs
@@ -6,13 +6,24 @@ using System.Threading.Tasks;
 
 namespace TunesAPI
 {
+    public enum Genre
+    {
+        Pop, Rock, Blues, Folk, Dance, HipHop, Metal, Jazz, Country, Disco 
+    }
     public class Tunes
     {
         //ID autogenerates
         public int Id { get; set; }
+        public int IrishChart { get; set; }
+        public int GlobalChart { get; set; }
+        public String Title { get; set; }
         public String Artist { get; set; }
-        public String Song { get; set; }
         public String Album { get; set; }
-        public String Genre { get; set; }
+        public Genre Genre { get; set; }
+        public String AlbumCoverLink { get; set; }
+        public double Price { get; set; }
+        public String BuyLink { get; set; }
+        public DateTime Realsed { get; set; }
+        public String Duration { get; set; }
     }
 }
