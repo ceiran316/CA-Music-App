3c9a2f943d3a98fa11f8e747c5b656601ad84424
ceiran316 <x00132492@gmail.com>
2019-03-23 17:37:54 +0000

    template api methods moved

--- TunesAPI/TunesAPI/Controllers/TunesController.cs
+++ TunesAPI/TunesAPI/Controllers/TunesController.cs
@@ -420,30 +420,5 @@ namespace TunesAPI.Controllers
             //return bangers.OrderBy(c => c.IrishChart).Select(d => d.Title);
             return _context.SuggestedTunes.OrderByDescending(t => t.Count);
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
