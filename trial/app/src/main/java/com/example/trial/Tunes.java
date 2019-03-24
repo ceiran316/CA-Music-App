38170f51b7a757eb4d8cb40f1d3675e791908a28
antoneville <antoneville@hotmail.com>
2019-03-24 11:50:13 +0000

    Android Client

---
+++ trial/app/src/main/java/com/example/trial/Tunes.java
@@ -0,0 +1,33 @@
+package com.example.trial;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Tunes {
+    @SerializedName("id")
+    public int ID;
+    @SerializedName("irishChart")
+    public int IrishChart;
+    @SerializedName("title")
+    public String Title;
+    @SerializedName("artist")
+    public String Artist;
+    @SerializedName("album")
+    public String Album;
+    @SerializedName("genre")
+    public String Genre;
+    @SerializedName("albumCoverLink")
+    public String AlbumCoverLink;
+    @SerializedName("price")
+    public double Price;
+    @SerializedName("buyLink")
+    public String BuyLink;
+    @SerializedName("realsed")
+    public String Realsed;
+    @SerializedName("duration")
+    public String Duration;
+
+    public String toString()
+    {
+        return Title + " " + Artist + " " + Album;
+    }
+}
