f612bf208bfead7c9f2bf8f24cbe5a81d53ec051
antoneville <antoneville@hotmail.com>
2019-03-31 22:46:45 +0100

    Testing

--- trial/app/src/androidTest/java/com/example/trial/MainActivityTest.java
+++ trial/app/src/androidTest/java/com/example/trial/MainActivityTest.java
@@ -1,13 +1,32 @@
 package com.example.trial;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import androidx.test.espresso.Espresso;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class MainActivityTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
     @Test
     public void onCreate() {
+        //Espresso.onView(withId(R.id.purchase)).perform(click());
     }
 
     @Test
